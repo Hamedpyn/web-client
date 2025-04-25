15470aa57b1e678ac936296045f1daa5e77f65fa
Hamed <hamedpyn@gmail.com>
2025-04-25 17:29:58 +0430

    Theme color changes

--- src/Components/NotExistedArray/NotExistedArray.jsx
+++ src/Components/NotExistedArray/NotExistedArray.jsx
@@ -1,7 +1,7 @@
 
 export default function NotExistedArray({msg,course}) {
     return (
-        <div className={`dark:bg-[#242A38] bg-white rounded p-4 lg:p-6 border-r-[#ef4444] border-r-[10px] dana-demi ${course && " m-3 md:m-5"}`}>
+        <div className={`dark:bg-[#242A38] bg-white text-gray-700 dark:text-white rounded p-4 lg:p-6 border-r-[#ef4444] border-r-[10px] dana-demi ${course && " m-3 md:m-5"}`}>
             {msg}
         </div>
     )
