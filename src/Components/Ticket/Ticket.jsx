d7dd421dcabe84cb5094d4aa3e4d7cc3774d130d
Hamed <hamedpyn@gmail.com>
2025-04-25 17:36:15 +0430

    Theme color changes

--- src/Components/Ticket/Ticket.jsx
+++ src/Components/Ticket/Ticket.jsx
@@ -7,7 +7,7 @@ export default function Ticket(tickets) {
     return (
         <Link to={`/my-account/Tickets/${tickets._id}`} className="dark:bg-[#242A38] bg-white rounded p-4 lg:p-6 flex justify-between gap-5 items-end flex-wrap">
             <div className="flex md:flex-row md:justify-between flex-col gap-y-3 flex-1">
-                <h3 className="dana-demi text-wrap ">{tickets.title}</h3>
+                <h3 className="dana-demi text-wrap text-white dark:text-black ">{tickets.title}</h3>
                 <span className="text-slate-500 dana-regular dark:text-gray-400">{tickets.departmentID}</span>
             </div>
             <div className="flex gap-4">
